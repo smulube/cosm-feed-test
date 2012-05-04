1b25e044ed6a0e9449fc4d8058ce1ed8c0129d0f
Sam Mulube <sam@pachube.com>
2012-05-04 10:47:42 +0000

    Generate all formats semi sensibly
    * returns slightly random data by default
    * returns location if passed a location parameter
    * returns tags if passed a tags parameter (same tags applied to all
      datastreams)

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 app.random_value = function() {
   var date = new Date(),
       random_offset = (Math.random() - 0.5) * 2;
-  return (Math.sin(0.05236 * date.getMinutes()) * 20 + random_offset).toFixed(2);
+  return (Math.sin(0.02618 * date.getMinutes()) * 20 + random_offset).toFixed(2);
 }
 
 app.content_type = function(format) {
@@ -23,7 +23,11 @@ app.content_type = function(format) {
 app.csv_v1 = function(options) {
   var data = [];
   for(var i = 0; i < options.datastreams; i++) {
-    data.push(app.random_value());
+    if (options.random === "false") {
+      data.push(i);
+    } else {
+      data.push(app.random_value());
+    }
   }
   return data.join(",");
 }
@@ -31,7 +35,11 @@ app.csv_v1 = function(options) {
 app.csv_v2 = function(options) {
   var data = [];
   for(var i= 0; i < options.datastreams; i++) {
-    data.push("stream" + i + "," + app.random_value());
+    if (options.random === "false") {
+      data.push("stream" + i + "," + i);
+    } else {
+      data.push("stream" + i + "," + app.random_value());
+    }
   }
   return data.join("\n");
 }
@@ -41,16 +49,77 @@ app.xml = function(options) {
     + '<eeml xmlns="http://www.eeml.org/xsd/0.5.1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n'
     + '  version="0.5.1" xsi:schemaLocation="http://www.eeml.org/xsd 0.5.1 http://www.eeml.org/xsd/0.5.1/0.5.1.xsd">\n'
     + '<environment>\n'
-    + '<title>Automatic Feed</title>\n'
-    + '<description>Feed description</description>\n'
-    + '</environment>\n</eeml>\n';
+    + '<title>Test Automatic Feed</title>\n'
+    + '<description>Description of automatic feed</description>\n'
+
+  if (options.location) {
+    var latlng = options.location.split(/,/);
+
+    xml += '<location domain="physical">\n'
+      + '<lat>' + latlng[0] + '</lat>\n'
+      + '<lon>' + latlng[1] + '</lon>\n'
+      + '</location>\n';
+  }
+
+  for (var i=0; i < options.datastreams; i++) {
+    if (options.random === "false") {
+      xml += '<data id="stream ' + i + '>\n';
+      if (options.tags) {
+        var tags = options.tags.split(/,/);
+        for(var j=0; j < tags.length; j++) {
+          xml += '<tag>' + tags[j] + '</tag>\n';
+        }
+      }
+      xml += '<current-value>' + i + '</current-value>\n'
+        + '</data>\n';
+    } else {
+      xml += '<data id="stream ' + i + '>\n';
+
+      if (options.tags) {
+        var tags = options.tags.split(/,/);
+        for(var j=0; j < tags.length; j++) {
+          xml += '<tag>' + tags[j] + '</tag>\n';
+        }
+      }
+      xml += '<current-value>' + app.random_value() + '</current-value>\n'
+        + '</data>\n';
+    }
+  }
+    xml += '</environment>\n</eeml>\n';
     return xml;
 }
 
 app.json = function(options) {
-  return JSON.stringify({ title: "Automatic Feed",
-                   description: "Description of automatic feed",
-                   version: "1.0.0"});
+  var json = { title: "Test Automatic Feed",
+               description: "Description of automatic feed",
+               version: "1.0.0"};
+
+  if (options.location) {
+    var latlng = options.location.split(/,/);
+
+    json["location"] = { lat: latlng[0], lon: latlng[1], domain: "physical" };
+  }
+
+  json.datastreams = [];
+
+  for (var i = 0; i < options.datastreams; i++) {
+    var datastream_json = { id: "stream" + i};
+    if (options.random === "false") {
+      datastream_json["current_value"] = i;
+      if (options.tags) {
+        datastream_json["tags"] = options.tags.split(/,/);
+      }
+      json.datastreams.push(datastream_json);
+    } else {
+      datastream_json["current_value"] = app.random_value();
+      if (options.tags) {
+        datastream_json["tags"] = options.tags.split(/,/);
+      }
+      json.datastreams.push(datastream_json);
+    }
+  }
+
+  return JSON.stringify(json);
 }
 
 app.get("/", function(request, response) {
@@ -61,9 +130,11 @@ app.get("/feed", function(request, response) {
   var format = request.param('format', 'json'),
       options = {};
 
+  // extract parameters (with defaults if not present)
   options.datastreams = parseInt(request.param('datastreams', "1"));
   options.tags = request.param('tags', '');
   options.random = request.param('random', 'true'),
+  options.location = request.param('location', '');
 
   response.header('Content-Type', app.content_type(format));
 
